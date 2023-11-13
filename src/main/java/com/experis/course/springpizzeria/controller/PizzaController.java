f8e93cf768c09972adf7fdeb5b351e92e00c151f
Ninotro <ninotroia96@gmail.com>
2023-11-13 15:17:35 +0100

    Done without validation

--- src/main/java/com/experis/course/springpizzeria/controller/PizzaController.java
+++ src/main/java/com/experis/course/springpizzeria/controller/PizzaController.java
@@ -3,13 +3,18 @@ package com.experis.course.springpizzeria.controller;
 
 import com.experis.course.springpizzeria.model.Pizza;
 import com.experis.course.springpizzeria.repository.PizzaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -20,11 +25,70 @@ public class PizzaController {
 
 
     @GetMapping
-    public String index(Model model) {
-        List<Pizza> pizzaList = pizzaRepository.findAll();
-        model.addAttribute("pizzaList", pizzaList);
+    public String index(@RequestParam Optional<String> search,
+                        Model model) {
+        List<Pizza> PizzaList;
 
-        return "pizzaList";
+        if (search.isPresent()) {
+            // se il parametro di ricerca è presente filtro la lista dei libri
+            PizzaList = pizzaRepository.findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
+
+                    search.get(),
+                    search.get());
+        } else {
+            // altrimenti prendo tutti i libri non filtrati
+            // bookRepository recupera da database la lista di tutti i libri
+            PizzaList = pizzaRepository.findAll();
+        }
+
+        // passo al template la lista di libri
+        model.addAttribute("pizzaList", PizzaList);
+        return "pizzalist";
     }
 
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        // verifico se il risultato è presente
+        if (result.isPresent()) {
+            // passo al template l'oggetto Book
+            model.addAttribute("pizza", result.get());
+            return "show";
+        } else {
+            // se non ho trovato il libro sollevo un'eccezione
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
+
+    }
+
+    //    metodo per mostrare il form
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("Pizza", new Pizza());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String doCreate(@Valid @ModelAttribute("pizza") Pizza formPizza,
+                           BindingResult bindingResult) {
+
+
+        // validare che i dati siano corretti
+        if (bindingResult.hasErrors()) {
+            // ci sono errori, devo ricaricare il form
+            return "create";
+        }
+
+        Pizza savedPizza = null;
+        try {
+            savedPizza = pizzaRepository.save(formPizza);
+        } catch (RuntimeException e) {
+            // aggiungo un errore di validazione per isbn
+            bindingResult.addError(new FieldError("Pizza", "description", formPizza.getDescription(), false, null, null,
+                    "ISBN must be unique"));
+            // ti rimando alla pagina col form
+            return "create";
+        }
+        return "redirect:/pizzas/show/" + savedPizza.getId();
+    }
 }
--- src/main/java/com/experis/course/springpizzeria/repository/PizzaRepository.java
+++ src/main/java/com/experis/course/springpizzeria/repository/PizzaRepository.java
@@ -3,5 +3,10 @@ package com.experis.course.springpizzeria.repository;
 import com.experis.course.springpizzeria.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+    List<Pizza> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description);
+
 }
+
